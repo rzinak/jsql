3b2a03d2af721e897282ab3e69584ffcf107259e
Renan Araújo <awkrenan@gmail.com>
2025-12-04 09:20:35 -0300

    added lexer

---
+++ GRAMMAR.md
@@ -0,0 +1,83 @@
+## 1. Valid Queries
+```
+SELECT * FROM users
+SELECT name, age, city FROM users
+SELECT name, age FROM users WHERE age > 18
+SELECT name, email, age FROM users WHERE age >= 18 AND city = "Rio de Janeiro"
+SELECT name, age FROM users WHERE age > 18 ORDER BY age DESC
+```
+
+---
+
+## 2. Tokens
+
+### Keywords
+```
+SELECT
+FROM
+WHERE
+ORDER
+BY
+```
+
+### Logical Operators
+``` AND
+OR
+```
+
+### Direction Keywords
+```
+ASC
+DESC
+```
+
+### Comparison Operators
+```
+=
+>
+
+>=
+<=
+!=
+```
+
+### Special Symbols
+```
+*
+,
+```
+
+### Identifiers
+Names defined by the user:
+- Column names: `name`, `age`, `email`, `city`
+- Table names: `users`, `data`
+
+**Pattern**: `[a-zA-Z_][a-zA-Z0-9_]*`
+
+### Literals
+
+**Numbers**
+- Exemplos: `18`, `21`, `100`
+- Pattern: `\d+` or `\d+\.\d+`
+
+**Strings**
+- Examples: `"Rio de Janeiro"`, `"Flamengo"`
+- Pattern: `"[^"]*"` (basically anything inside quotes)
+
+### Whitespace
+- Whitespaces, tabs and break-line
+- **Ignored during parsing**
+
+---
+
+## 3. Formal Grammar
+```
+Query      -> SELECT Columns FROM Table [WHERE Conditions] [ORDER BY OrderClause]
+Columns    -> * | ColumnList
+ColumnList -> Identifier (, Identifier)*
+Conditions -> Condition ((AND | OR) Condition)*
+Condition  -> Identifier Operator Literal
+Operator   -> = | > | < | >= | <= | !=
+Literal    -> Number | String
+OrderClause-> Identifier [Direction]
+Direction  -> ASC | DESC
---
+++ src/engine/lexer.ts
@@ -0,0 +1,137 @@
+// {
+//   type: 'KEYWORD', // ou 'IDENTIFIER', 'NUMBER', 'STRING', 'OPERATOR', 'SYMBOL'
+//   value: 'SELECT'  // ou 'age', '18', 'Rio', '>=', ','
+// }
+
+type TokenType =
+  | 'KEYWORD'
+  | 'IDENTIFIER'
+  | 'NUMBER'
+  | 'STRING'
+  | 'OPERATOR'
+  | 'SYMBOL'
+
+type Token = {
+  type: TokenType;
+  value: string;
+}
+
+const KEYWORDS = [
+  "SELECT",
+  "FROM",
+  "WHERE",
+  "ORDER",
+  "BY",
+  "AND",
+  "OR",
+  "NOT"
+];
+
+const tokenize = (query: string): Token[] => {
+  let tokens: Token[] = [];
+
+  let current = 0;
+
+  const peekChar = (): string => query[current + 1];
+
+  const isDigit = (char: string): boolean => /\d/.test(char);
+
+  const isLetter = (char: string): boolean => /^[a-z_]$/i.test(char);
+
+  // @ts-ignore
+  const peek = (): Token => tokens[current];
+
+  // @ts-ignore
+  const isAtEnd = (): boolean => current >= tokens.length;
+
+  // @ts-ignore
+  const advance = (): Token => {
+    if (!isAtEnd()) {
+      current++;
+    }
+    return tokens[current - 1];
+  }
+
+  while (current < query.length) {
+    const char = query[current];
+    if (char === ' ' || char === '\t') {
+      current++;
+      continue;
+    }
+
+    if (',*'.includes(char)) {
+      tokens.push({ type: 'SYMBOL', value: char });
+      current++;
+      continue;
+    }
+
+    if ('=><!'.includes(char)) {
+      if (char === '>' && peekChar() === '=') {
+        tokens.push({ type: 'OPERATOR', value: '>=' });
+        current += 2;
+        continue;
+      }
+
+      if (char === '<' && peekChar() === '=') {
+        tokens.push({ type: 'OPERATOR', value: '<=' });
+        current += 2;
+        continue;
+      }
+
+      if (char === '!' && peekChar() === '=') {
+        tokens.push({ type: 'OPERATOR', value: '!=' });
+        current += 2;
+        continue;
+      }
+
+      tokens.push({ type: 'OPERATOR', value: char });
+      current++;
+      continue;
+    }
+
+    if (char === '"') {
+      current++;
+      let val: string = '';
+      while (query[current] !== '"') {
+        val += query[current];
+        current++;
+      }
+      tokens.push({ type: 'STRING', value: val });
+      current++;
+      continue;
+    }
+
+    if (isLetter(char)) {
+      let val: string = '';
+      while (isLetter(query[current]) || isDigit(query[current])) {
+        console.log(query[current])
+        val += query[current];
+        current++;
+      }
+      if (KEYWORDS.includes(val)) {
+        tokens.push({ type: 'KEYWORD', value: val });
+      } else {
+        tokens.push({ type: 'IDENTIFIER', value: val });
+      }
+      continue;
+    }
+
+    if (isDigit(char)) {
+      let val: string = '';
+      while (isDigit(query[current]) || query[current] === '.') {
+        val += query[current];
+        current++;
+      }
+      tokens.push({ type: 'NUMBER', value: val });
+      continue;
+    }
+
+    current++;
+  }
+
+  return tokens;
+}
+
+const query = 'SELECT name, age FROM users WHERE age >= 18';
+const t = tokenize(query);
+console.log(t);
